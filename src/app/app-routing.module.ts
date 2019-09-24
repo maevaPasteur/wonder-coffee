639d382d8ca8c86662158cbb91479c0bc6efa991
Maeva Pasteur <maeva.pasteur@hetic.net>
2019-09-24 14:08:20 +0200

    Feat routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,23 @@ import {Routes, RouterModule} from '@angular/router';
 
 import {HomeComponent} from './components/global/home/home.component';
 import {SizeComponent} from './components/steps/size/size.component';
+import { ProductCategoryComponent } from './product-category/product-category.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { IntensityComponent } from './intensity/intensity.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { ToppingsComponent } from './toppings/toppings.component';
+import { OrderComponent } from './order/order.component';
 
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'taille', component: SizeComponent}
+    {path: 'taille', component: SizeComponent},
+    {path: 'categorie', component: ProductCategoryComponent},
+    {path: 'produit', component: ProductItemComponent},
+    {path: 'intensite', component: IntensityComponent},
+    {path: 'ingredients', component: IngredientsComponent},
+    {path: 'nappage', component: ToppingsComponent},
+    {path: 'commande', component: OrderComponent},
 ];
 
 @NgModule({
