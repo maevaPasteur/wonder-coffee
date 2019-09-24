73595d6901849572c5f13d9c701502f7874e11c1
Maeva Pasteur <maeva.pasteur@hetic.net>
2019-09-25 00:00:42 +0200

    Feat product & intensity

--- src/app/components/steps/intensity/intensity.component.ts
+++ src/app/components/steps/intensity/intensity.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {DataService} from '../../../services/data.service';
 
 @Component({
-  selector: 'app-intensity',
-  templateUrl: './intensity.component.html',
-  styleUrls: ['./intensity.component.sass']
+    selector: 'app-intensity',
+    templateUrl: './intensity.component.html',
+    styleUrls: ['./intensity.component.sass']
 })
 export class IntensityComponent implements OnInit {
 
-  constructor() { }
+    intensity: any;
 
-  ngOnInit() {
-  }
+    @Input() inputData: any;
+    @Output() sendBackData = new EventEmitter();
+
+    constructor(private data: DataService) {
+    }
+
+    ngOnInit() {
+        // this.data.getProducts().subscribe(data => {
+        //         this.product = data[0].category.find(c => c.name === this.inputData.product_category).products;
+        //         console.log(this.product);
+        //     }
+        // );
+        this.data.getIntensity().subscribe(data => {
+                this.intensity = data;
+                console.log(this.intensity);
+            }
+        );
+    }
 
 }
